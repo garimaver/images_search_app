40338ec78703098ecafd6d52da7ebb0dee876200
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-03 14:56:01 +0530

    ADDED JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const accessKey = "NNYntjsVVUiAhVHrNHV8RFghJFXuHePGK2NbJDAgMx4"
+const accessKey = "NNYntjsVVUiAhVHrNHV8RFghJFXuHePGK2NbJDAgMx4";
 
 
 const formE1 = document.querySelector("form")
@@ -6,42 +6,57 @@ const inputE1 = document.getElementById("search-input")
 const searchResults = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
 
-let inputData = ""
+let inputData = "";
 let page = 1;
 
  async function searchImages(){
     inputData = inputE1.Value;
-    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-const response = await fetch(url)
-const data = await response.json()
+const response = await fetch(url);
+const data = await response.json();
 
-const results = data.results
+const results = data.results;
 
 
 if (page === 1){
-    searchResults.innerHTML = ""
+    searchResults.innerHTML = "";
 }
 
-results.map((result)=>{
-const imageWrapper = document.createElement('div')
-imageWrapper.classList.add("search-result")
-const image = document.createElement('img')
-image.src = result.urls.small
-image.alt= result.alt.description
-const imageLink = document.createElement('a')
-imageLink.href = result.links.html
-imageLink.target = "_blank"
-imageLink.textContent= result.alt.description
+results.map((result) => {
+const imageWrapper = document.createElement("div");
+imageWrapper.classList.add("search-result");
+const image = document.createElement("img");
+image.src = result.urls.small;
+image.alt= result.alt_description;
+const imageLink = document.createElement("a");
+imageLink.href = result.links.html;
+imageLink.target = "_blank";
+imageLink.textContent= result.alt_description;
 
 
 imageWrapper.appendChild(image);
 imageWrapper.appendChild(imageLink);
-imageWrapper.appendChild(imageWrapper);
-})
- page ++
+searchResults.appendChild(imageWrapper);
+});
+
+
+ page++;
  if(page > 1){
-    showMore.style.display ="block"
+    showMore.style.display ="block";
  }
 
 }
+
+formE1.addEventListener("submit", (event)=>{
+event.preventDefault();
+page = 1;
+searchImages();
+
+});
+
+showMore.addEventListener("click", ()=>{
+    searchImages();
+    
+    });
+
