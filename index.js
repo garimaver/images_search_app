47304c7d2927de097179450b73557382bbf48c07
garima verma <114860130+garimaver@users.noreply.github.com>
2023-09-03 13:48:09 +0530

    added api

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const accessKey = "NNYntjsVVUiAhVHrNHV8RFghJFXuHePGK2NbJDAgMx4"
+
+
+const formE1 = document.querySelector("form")
+const inputE1 = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
+
+ async function searchImages(){
+    inputData = inputE1.Value;
+    const url= `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+const response = await fetch(url)
+const data = await response.json()
+
+const results = data.results
+
+
+if (page === 1){
+    searchResults.innerHTML = ""
+}
+
+results.map((result)=>{
+const imageWrapper = document.createElement('div')
+imageWrapper.classList.add("search-result")
+const image = document.createElement('img')
+image
+})
+}
